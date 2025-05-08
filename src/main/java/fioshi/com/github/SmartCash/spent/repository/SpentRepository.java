66f6f21230238ee5a316eb1e03a8f2e7a1e2b105
Fioshi <guilhermefiochi@outlook.com>
2025-05-08 09:35:29 -0300

    docs: adicionado documentacao de endpoints atuais

--- src/main/java/fioshi/com/github/SmartCash/spent/repository/SpentRepository.java
+++ src/main/java/fioshi/com/github/SmartCash/spent/repository/SpentRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SpentRepository extends JpaRepository<Spent, Long> {
-
     Optional<Spent> findByIdAndUserId(Long id, Long userId);
-
 }
